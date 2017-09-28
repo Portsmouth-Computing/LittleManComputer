bdd55cb801408b95395c9b4c61df7c614e43f152
Matthew Hopson <mhopson@hotmail.co.uk>
2017-09-28 13:32:49 +0100

    init commit

---
+++ lmc.py
@@ -0,0 +1,75 @@
+memory = [] 
+
+
+progCounter = 0
+instructionRegister = -1
+addressRegister = 0
+accumulator = 0
+
+def loadInstruction(op, strAddress, location):
+    op = str(op)
+    op += strAddress
+    memory[location] = int(op)
+
+def loadInstructions():
+    inFile = open("instructions.txt")
+    
+    instructionPtr = 0
+    for line in inFile:
+        words = line.split(" ")
+        instruction = words[0]
+        if (instruction == "LDA"):
+            loadInstruction(5, words[1], instructionPtr)
+        elif(instruction == "STA"):
+            loadInstruction(3, words[1], instructionPtr)
+        elif (instruction == "ADD"):
+            loadInstruction(1, words[1], instructionPtr)
+        elif (instruction == "SUB"):
+            loadInstruction(2, words[1], instructionPtr)
+        elif (instruction == "INP\n"):
+            loadInstruction(9, "01", instructionPtr)
+        elif (instruction == "OUT\n"):
+            loadInstruction(9, "02", instructionPtr)
+        elif (instruction == "HTL\n"): 
+            loadInstruction(0, "00", instructionPtr)
+        instructionPtr += 1
+        
+    inFile.close()
+    
+    for instruction in memory:
+        print (instruction)
+        if instruction == 0:
+            break;
+        
+
+        
+def run():
+    print ("\n RUNNING \n")
+    while instructionRegister != 0:
+        instruction = memory[progCounter]
+        instrStr = str(instruction)
+        print (instruction)
+        progCounter += 1
+        
+    
+    
+
+def main():
+    #init the memory
+    for i in range (99):
+        memory.append(0)
+        
+    #load the instructions
+    loadInstructions();
+    
+    #run the program
+    run()
+    
+    
+    
+    
+
+    
+    
+if __name__ == "__main__":
+    main()
