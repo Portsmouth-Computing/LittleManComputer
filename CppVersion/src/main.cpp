6363329c3dc2c2eeef018566a3a71b9dd1ff1c1c
Hopson97 <mhopson@hotmail.co.uk>
2017-10-01 11:12:12 +0100

    c++ window opening

--- CppVersion/src/main.cpp
+++ CppVersion/src/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
+void handleWindowEvents(sf::RenderWindow& window)
+{
+    sf::Event e;
+    while (window.pollEvent(e))
+    {
+        switch(e.type)
+        {
+            case sf::Event::Closed:
+                window.close();
+                break;
+
+            default:
+                break;
+        }
+    }
+}
 
 int main()
 {
+    sf::RenderWindow window({1280, 720}, "LittleManComputer");
+
+    while (window.isOpen())
+    {
+        window.clear();
+
+
+        window.display();
+        handleWindowEvents(window);
+    }
+
 
 }
