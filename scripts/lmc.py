50c31f4452db2a34ad65e2c5fb035d62d686b6a8
Sam Mercer <sam@mercerfamily.org.uk>
2017-10-17 10:42:02 +0100

    autopep8

--- scripts/lmc.py
+++ scripts/lmc.py
@@ -4,10 +4,11 @@ from lmc_parser import LMCParser
 
 class Window(Frame):
     """LMC"""
+
     def __init__(self, master=None):
         Frame.__init__(self, master)
         #Frame.configure(self, bg = 'black')
-        Frame.grid_columnconfigure(self, 3, minsize=20)#Add a spacer
+        Frame.grid_columnconfigure(self, 3, minsize=20)  # Add a spacer
         self.master = master
         self.reset_everything()
 
@@ -19,14 +20,15 @@ class Window(Frame):
         """Loads Instructions Into Memory"""
         self.reset_everything()
 
-
         instructionList = self.textarea.get(1.0, END)
         instructionList.upper()
-        instructionList = instructionList.split("\n") #split the string into individual instructions
-        instructionList.pop(len(instructionList) - 1) #get rid of the final char, which is random space for some reason
+        # split the string into individual instructions
+        instructionList = instructionList.split("\n")
+        # get rid of the final char, which is random space for some reason
+        instructionList.pop(len(instructionList) - 1)
         asmler = LMCParser()
         asmler.assemble(instructionList, self.memory)
-        #update the GUI
+        # update the GUI
         self.update_memory()
 
     def run(self):
@@ -62,40 +64,41 @@ class Window(Frame):
                 lmcInput()
             elif self.address_register == 2:
                 lmcOutput()
-    
+
         def lmcInput():
             self.var = IntVar()
-            self.inputarea = Text(self, height = 1, width = 5)
-            self.inputbutton = Button(self, text = "Confirm",command=lambda: self.var.set(1))
-            self.inputarea.grid(row = 16, column = 4)
-            self.inputbutton.grid(row = 16, column = 5)
+            self.inputarea = Text(self, height=1, width=5)
+            self.inputbutton = Button(
+                self, text="Confirm", command=lambda: self.var.set(1))
+            self.inputarea.grid(row=16, column=4)
+            self.inputbutton.grid(row=16, column=5)
 
             self.inputbutton.wait_variable(self.var)
             #print("Processing", self.var)
-            self.tempvar = self.inputarea.get(1.0,END)
+            self.tempvar = self.inputarea.get(1.0, END)
             self.accumulator = int(self.tempvar)
             self.inputarea.delete(1.0, END)
             self.update_counters()
             self.update_memory()
 
         instruction_list = [
-            lmcAdd,     #1
-            lmcSub,     #2
-            lmcStore,   #3
-            lmcIO,      #4
-            lmcLoad,    #5
-            lmcBranchAlways,    #6
-            lmcBranchIfZero,    #7
-            lmcBranchIfZeroOrPositive,  #8
-            lmcIO]      #8
+            lmcAdd,  # 1
+            lmcSub,  # 2
+            lmcStore,  # 3
+            lmcIO,  # 4
+            lmcLoad,  # 5
+            lmcBranchAlways,  # 6
+            lmcBranchIfZero,  # 7
+            lmcBranchIfZeroOrPositive,  # 8
+            lmcIO]  # 8
 
         while self.instruction_register != 0:
-            #split instructions into instruction and address
-            #bus would move to address, take into cpu registers
-            instr   = str(self.memory[self.program_counter])
+            # split instructions into instruction and address
+            # bus would move to address, take into cpu registers
+            instr = str(self.memory[self.program_counter])
             if int(instr) == 0:
                 break
-            opcode  = instr[0]
+            opcode = instr[0]
 
             if len(instr) == 3:
                 address = instr[1] + instr[2]
@@ -104,13 +107,12 @@ class Window(Frame):
 
             self.program_counter += 1
 
-            #push to registers
+            # push to registers
             self.instruction_register = int(opcode)
-            self.address_register     = int(address)
+            self.address_register = int(address)
 
             #print("OpCode: ", self.program_counter, self.instruction_register, self.address_register)
             instruction_list[self.instruction_register - 1]()
-
 
             '''
             
@@ -143,101 +145,109 @@ class Window(Frame):
         self.master.title("LMC")
         self.pack(fill=BOTH, expand=1)
         self.header_label = Label(self, text="Little Man Computer")
-        self.header_label.grid(row = 0, column = 0, columnspan = 3)
+        self.header_label.grid(row=0, column=0, columnspan=3)
         # Create text box
-        self.textarea = Text(self,width = 20, height = 35)
+        self.textarea = Text(self, width=20, height=35)
         #self.textarea.pack(expand=NO, fill ="y")
-        self.textarea.grid(row = 1, column = 0, columnspan = 3, rowspan = 20)
+        self.textarea.grid(row=1, column=0, columnspan=3, rowspan=20)
 
-        self.execute_button = Button(self, text = "Execute", command = self.run)
-        self.execute_button.grid(row = 22, column = 0)
-        self.reset_button = Button(self, text = "Reset", command = self.reset)
-        self.reset_button.grid(row = 22, column = 1)
-        self.burn_it_all = Button(self, text = "Exit", command = self.exit)
-        self.burn_it_all.grid(row = 22, column = 2)
-        self.assemble = Button(self, text = "Load", command = self.load_instructions)
-        self.assemble.grid(row = 23, column = 0)
+        self.execute_button = Button(self, text="Execute", command=self.run)
+        self.execute_button.grid(row=22, column=0)
+        self.reset_button = Button(self, text="Reset", command=self.reset)
+        self.reset_button.grid(row=22, column=1)
+        self.burn_it_all = Button(self, text="Exit", command=self.exit)
+        self.burn_it_all.grid(row=22, column=2)
+        self.assemble = Button(
+            self, text="Load", command=self.load_instructions)
+        self.assemble.grid(row=23, column=0)
 
-    def update_counters(self):       
+    def update_counters(self):
         self.accvar = IntVar()
         self.accvar.set(self.accumulator)
-        self.accumulator_label = Label(self,text="Accumulator")
-        self.accumulator_label.grid(row = 1, column = 4)
-        self.accumulator_value = Label(self, textvariable = self.accvar, bg = 'grey')
-        self.accumulator_value.grid(row = 2, column = 4)
+        self.accumulator_label = Label(self, text="Accumulator")
+        self.accumulator_label.grid(row=1, column=4)
+        self.accumulator_value = Label(
+            self, textvariable=self.accvar, bg='grey')
+        self.accumulator_value.grid(row=2, column=4)
 
         self.progvar = IntVar()
         self.progvar.set(self.program_counter)
         self.prog_label = Label(self, text="Progress")
         self.prog_label.grid(row=4, column=4)
-        self.prog_value = Label(self, textvariable = self.progvar, bg = 'grey')
-        self.prog_value.grid(row = 5, column = 4)
+        self.prog_value = Label(self, textvariable=self.progvar, bg='grey')
+        self.prog_value.grid(row=5, column=4)
 
         self.addressvar = IntVar()
         self.addressvar.set(self.address_register)
         self.address_label = Label(self, text="Current Address")
-        self.address_label.grid(row = 7, column = 4)
-        self.address_value = Label(self, textvariable = self.addressvar, bg = 'grey')
-        self.address_value.grid(row = 8, column = 4)
+        self.address_label.grid(row=7, column=4)
+        self.address_value = Label(
+            self, textvariable=self.addressvar, bg='grey')
+        self.address_value.grid(row=8, column=4)
 
         self.instruction_value = IntVar()
         self.instruction_value.set(self.instruction_register)
-        self.instruction_label = Label(self, text = "Instruction Register")
-        self.instruction_label.grid(row = 10, column = 4)
-        self.instruction_value = Label(self, textvariable = self.instruction_value, bg = 'grey')
-        self.instruction_value.grid(row = 11, column = 4)
+        self.instruction_label = Label(self, text="Instruction Register")
+        self.instruction_label.grid(row=10, column=4)
+        self.instruction_value = Label(
+            self, textvariable=self.instruction_value, bg='grey')
+        self.instruction_value.grid(row=11, column=4)
 
     def update_output(self):
         self.output_value = IntVar()
         self.output_value.set(self.accumulator)
-        self.output_label = Label(self, text = "Output")
-        self.output_label.grid(row = 13, column = 4)
-        self.output_value = Label(self, textvariable = self.output_value, bg = 'grey')
-        self.output_value.grid(row = 14, column = 4)
+        self.output_label = Label(self, text="Output")
+        self.output_label.grid(row=13, column=4)
+        self.output_value = Label(
+            self, textvariable=self.output_value, bg='grey')
+        self.output_value.grid(row=14, column=4)
         self.update_counters()
 
     def update_memory(self):
-        for x in range(0,10):
-            for y in range(0,20):
+        for x in range(0, 10):
+            for y in range(0, 20):
                 var = StringVar()
                 value_var = StringVar()
                 if y % 2 == 0:
                     loc_var = int((y / 2) * 10 + x)
                     var.set("Address " + str(loc_var))
-                    self.memory_label = Label(self, textvariable = var)
-                    self.memory_label.grid(row = 1 +(y), column = 6+(x),ipadx = 5, ipady = 2 )
+                    self.memory_label = Label(self, textvariable=var)
+                    self.memory_label.grid(
+                        row=1 + (y), column=6 + (x), ipadx=5, ipady=2)
                 else:
                     value_var.set(self.memory[loc_var])
-                    self.memory_value = Label(self, textvariable = value_var, bg = 'grey')
-                    self.memory_value.grid(row = 1 +(y), column = 6+(x))
+                    self.memory_value = Label(
+                        self, textvariable=value_var, bg='grey')
+                    self.memory_value.grid(row=1 + (y), column=6 + (x))
 
     def reset(self):
         self.textarea.delete(1.0, END)
         self.reset_everything()
 
     def reset_everything(self):
-        self.memory                 = []
-        self.program_counter        = 0
-        self.instruction_register   = -1
-        self.address_register       = 0
-        self.accumulator            = 0
-        for i in range (100):
+        self.memory = []
+        self.program_counter = 0
+        self.instruction_register = -1
+        self.address_register = 0
+        self.accumulator = 0
+        for i in range(100):
             self.memory.append(0)
         self.update_counters()
         self.update_memory()
         self.update_output()
 
-
     def exit(self):
         exit()
 
+
 def main():
-    #Creates UI and starts
+    # Creates UI and starts
     root = Tk()
     root.geometry("1100x650")
     app = Window(master=root)
-    #app.update_memory()
+    # app.update_memory()
     app.mainloop()
+
 
 if __name__ == "__main__":
     main()
--- scripts/lmc_parser.py
+++ scripts/lmc_parser.py
@@ -1,20 +1,22 @@
 def remove_empty(arr):
     arr = filter(None, arr)
 
+
 class LMCParser:
     '''Class to parse LMC instructions and assemble into machine code'''
+
     def __init__(self):
-        self.commands = {   "HTL": 0, 
-                            "ADD": 1,
-                            "SUB": 2,
-                            "STA": 3,
-                            "LDA": 5,
-                            "BRA": 6,
-                            "BRZ": 7,
-                            "BRP": 8,
-                            "INP": 9,
-                            "OUT": 9,
-                            "DAT": 4}
+        self.commands = {"HTL": 0,
+                         "ADD": 1,
+                         "SUB": 2,
+                         "STA": 3,
+                         "LDA": 5,
+                         "BRA": 6,
+                         "BRZ": 7,
+                         "BRP": 8,
+                         "INP": 9,
+                         "OUT": 9,
+                         "DAT": 4}
         self.labels = dict()
 
     def get_labels(self, command_list):
@@ -24,7 +26,7 @@ class LMCParser:
             words = line.split(" ")
             remove_empty(words)
             if not words[0] in self.commands:
-                self.labels[words[0]] = instruction_ptr #insert label
+                self.labels[words[0]] = instruction_ptr  # insert label
             instruction_ptr += 1
 
     def load_instruction(self, memory, op_code, ref_address, mem_location):
@@ -43,21 +45,25 @@ class LMCParser:
         if instruction == "INP":
             self.load_instruction(memory, 9, "01", memory_location)
         elif instruction == "OUT":
-            self.load_instruction(memory, 9, "02", memory_location) #INP, OUT, and HLT are special cases, as they do not have an address to manip
+            # INP, OUT, and HLT are special cases, as they do not have an address to manip
+            self.load_instruction(memory, 9, "02", memory_location)
         elif instruction == "HLT":
             self.load_instruction(memory, 0, "00", memory_location)
         else:
             if instruction in self.commands:
-                self.load_instruction(memory, self.commands[instruction], line[1], memory_location)
+                self.load_instruction(
+                    memory, self.commands[instruction], line[1], memory_location)
             else:
-                if (line[1] == "DAT"): #Dat, takes the form of NAME DAT INITAL VALUE
+                if (line[1] == "DAT"):  # Dat, takes the form of NAME DAT INITAL VALUE
                     if(len(line) == 2):
-                        memory[memory_location] = 0 #a value is optional, so default to 0
-                    else: 
-                        memory[memory_location] = int(line[2])    
-                else: #this means it is a label, so pop that out and reparse the line
+                        # a value is optional, so default to 0
+                        memory[memory_location] = 0
+                    else:
+                        memory[memory_location] = int(line[2])
+                else:  # this means it is a label, so pop that out and reparse the line
                     line.pop(0)
-                    self.parse_line(line, memory_location, memory)  #recursion :^)
+                    # recursion :^)
+                    self.parse_line(line, memory_location, memory)
 
     def assemble(self, command_list, memory):
         '''Take the list of commands and converts it into Op code in memory'''
